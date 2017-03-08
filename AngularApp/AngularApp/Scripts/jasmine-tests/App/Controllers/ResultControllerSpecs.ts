d374709f5d4303ff88f0ecd94afb5ec684e5fa80
foleq <piotrekgda90@gmail.com>
2017-03-08 19:45:06 +0100

    Add jasmine test using typescript

---
+++ AngularApp/AngularApp/Scripts/jasmine-tests/App/Controllers/ResultControllerSpecs.ts
@@ -0,0 +1,15 @@
+﻿module App.Controllers.Tests {
+
+    describe("ResultController", () => {
+
+        var resultController: ResultController;
+
+        beforeEach(() => {
+            resultController = new ResultController();
+        });
+
+        it("should contain correct title", () => {
+            expect(resultController.title).toBe("Hello world!");
+        });
+    });
+}
